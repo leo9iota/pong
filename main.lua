f4618f93ae763a0ca4c7a6e8f918cb3c2a5f7a5a
leo9iota <leon20.zh@gmail.com>
2023-03-22 17:02:11 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,37 @@
+push = require "push"
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 640
+VIRTUAL_HEIGHT = 360
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
+    pixelFontSmall = love.graphics.newFont("public-pixel-font.ttf", 8)
+    love.graphics.setFont(pixelFontSmall)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown("w") then
+        print("key w")
+    elseif love.keyboard.isDown("s") then
+        print("key s")
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown("up") then
+        print("key up")
+    elseif love.keyboard.isDown("down") then
+        print("key down")
+            
+    end
+end

b0113fefa9c88548f32962562d7983e040ae43a6
leo9iota <leon20.zh@gmail.com>
2023-04-07 14:38:36 +0200

    Add background

---
+++ Background.lua
@@ -0,0 +1,13 @@
+Background = {}
+
+function Background:load()
+    self.tennisCourt = love.graphics.newImage("")
+end
+
+function Background:update(dt)
+    
+end
+
+function Background:draw()
+    
+end
